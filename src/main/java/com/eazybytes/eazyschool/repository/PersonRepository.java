fb7564666740686c80660a6b94b3ca49c3096ccc
kashish0kanojia <kashish.kanojia@ucdconnect.ie>
2024-07-10 17:38:55 +0100

    Adding new files

---
+++ src/main/java/com/eazybytes/eazyschool/repository/PersonRepository.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eazyschool.repository;
+
+import com.eazybytes.eazyschool.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+    Person readByEmail(String email);
+
+}
