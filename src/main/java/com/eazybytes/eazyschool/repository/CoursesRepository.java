7844980277867dec4690132a6b7519120f1b51b8
kashish0kanojia <kashish.kanojia@ucdconnect.ie>
2024-07-10 17:28:17 +0100

    eazySchool web application

---
+++ src/main/java/com/eazybytes/eazyschool/repository/CoursesRepository.java
@@ -0,0 +1,27 @@
+package com.eazybytes.eazyschool.repository;
+
+import com.eazybytes.eazyschool.model.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RepositoryRestResource(path="courses",exported = false)
+public interface CoursesRepository extends JpaRepository<Courses, Integer> {
+
+    /*
+    The Asc keyword is optional as OrderBy, by default,
+    sorts the results in the ascending order.
+    * */
+    List<Courses> findByOrderByName();
+
+
+
+    /*
+        Spring Data JPA allows us to apply static sorting by adding the OrderBy keyword
+        to the method name along with the property name and sort direction (Asc or Desc).
+        * */
+    List<Courses> findByOrderByNameDesc();
+}
