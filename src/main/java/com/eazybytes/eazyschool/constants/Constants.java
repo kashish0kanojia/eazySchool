754b69f246a8a2050268f2e55982c3dc95cb8301
kashish0kanojia <kashish.kanojia@ucdconnect.ie>
2024-07-10 17:38:11 +0100

    Changes

--- src/main/java/com/eazybytes/eazyschool/constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.eazybytes.eazyschool.constants;
-
-public class Constants {
-    public static final String OPEN = "Open";
-    public static final String CLOSE = "Close";
-    public static final String STUDENT_ROLE = "STUDENT";
-    public static final String ADMIN_ROLE = "ADMIN";
-}
